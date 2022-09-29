310bb2d849663682d2f97857e9a6daafbc80688e
Micaela Iacono <iaconomicaela@gmail.com>
2022-09-29 20:24:23 -0300

    pre-entrega

---
+++ primer-proyecto/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button';
+
+export const NavBar = ()=>{
+    return(
+        <div>
+            menu de navegación
+            <Button variant="success" size="sm">Botón de bootstrap</Button>
+        </div>
+    )
+}
